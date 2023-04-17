f1b391301402348acf67c6887a0eb40981e51294
jonator <jonathanator0@gmail.com>
2023-04-17 12:50:09 -0700

    rename + fix build script

--- scripts/1-get-sdk-proto.sh
+++ scripts/1-get-sdk-proto.sh
@@ -7,11 +7,11 @@ COSMOS_DIR="$PROTO_DIR/cosmos"
 COSMOS_SDK_DIR="$COSMOS_DIR/cosmos-sdk"
 ZIP_FILE="$COSMOS_DIR/tmp.zip"
 
-COSMOS_SDK_VERSION="v0.47.1"
+COSMOS_SDK_VERSION="0.47.1"
 
 mkdir -p "$COSMOS_SDK_DIR"
 
-wget -qO "$ZIP_FILE" "https://github.com/cosmos/cosmos-sdk/archive/$COSMOS_SDK_VERSION.zip"
+wget -qO "$ZIP_FILE" "https://github.com/cosmos/cosmos-sdk/archive/v$COSMOS_SDK_VERSION.zip"
 unzip "$ZIP_FILE" "*.proto" -d "$COSMOS_DIR"
 mv "$COSMOS_SDK_DIR-$COSMOS_SDK_VERSION" "$COSMOS_SDK_DIR"
 rm "$ZIP_FILE"

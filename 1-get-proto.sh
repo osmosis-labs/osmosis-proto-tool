710ed81377b6104f717f2500a08455306baf54d4
jonator <jonathanator0@gmail.com>
2023-04-20 09:58:08 -0500

    fix paths, ignore repos folder

--- 1-get-proto.sh
+++ 1-get-proto.sh
@@ -27,7 +27,7 @@ else
 fi
 
 # Move the protos folder to the desired destination
-cp -i -f repos/cosmos-sdk/proto/cosmos "$COSMOS_SDK_DIR/proto/cosmos" || true
+cp -R repos/cosmos-sdk/proto/cosmos "$COSMOS_SDK_DIR/proto/cosmos" || true
 
 
 # OSMOSIS PROTOS
@@ -35,8 +35,8 @@ cp -i -f repos/cosmos-sdk/proto/cosmos "$COSMOS_SDK_DIR/proto/cosmos" || true
 # If the osmosis repo is there, fetch, otherwise clone sparse
 if [ -d "repos/osmosis/.git" ]; then
   git -C repos/osmosis fetch --all --tags
-  git -C repos/osmosis checkout master || git -C osmosis checkout main
-  git -C repos/osmosis pull origin master || git -C osmosis pull origin main
+  git -C repos/osmosis checkout main
+  git -C repos/osmosis pull origin main
 else
   git clone --filter=blob:none --sparse https://github.com/osmosis-labs/osmosis.git repos/osmosis
 
@@ -52,13 +52,13 @@ if [ $# -gt 0 ]; then
 fi
 
 # Move the osmosis/proto/osmosis folder to the desired destination
-cp -i -f repos/osmosis/proto/osmosis "$COSMOS_SDK_DIR/proto/osmosis" || true
+cp -R repos/osmosis/proto/osmosis "$COSMOS_SDK_DIR/proto/osmosis" || true
 
 
 # IBC PROTOS
 
 # Extract the IBC Go version from the go.mod file
-IBC_GO_VERSION=$(awk '/github.com\/cosmos\/ibc-go/ {print $2}' osmosis/go.mod)
+IBC_GO_VERSION=$(awk '/github.com\/cosmos\/ibc-go/ {print $2}' repos/osmosis/go.mod)
 
 # If the ibc-go repo is there, fetch, otherwise clone sparse
 if [ -d "repos/ibc-go/.git" ]; then
@@ -73,13 +73,13 @@ else
 fi
 
 # Move IBC Go proto files into the $COSMOS_SDK_DIR/proto directory
-cp -i -f repos/ibc-go/proto/* "$COSMOS_SDK_DIR/proto/" || true
+cp -R repos/ibc-go/proto/* "$COSMOS_SDK_DIR/proto/" || true
 
 
 # WASMD PROTOS
 
 # Extract the Wasmd version from the go.mod file
-WASMD_VERSION=$(awk '/github.com\/osmosis-labs\/wasmd/ {print $4}' osmosis/go.mod)
+WASMD_VERSION=$(awk '/github.com\/osmosis-labs\/wasmd/ {print $4}' repos/osmosis/go.mod)
 
 # Clone or update the wasmd repo (osmosis-labs fork)
 # If the wasmd repo is there, fetch, otherwise clone sparse
@@ -96,4 +96,4 @@ fi
 
 # Move Wasmd proto files into the $COSMOS_SDK_DIR/proto directory
 mkdir -p $COSMOS_SDK_DIR/proto/cosmwasm/
-cp -i -f repos/wasmd/proto/cosmwasm/wasm/v1/* "$COSMOS_SDK_DIR/proto/cosmwasm/" || true
+cp -R repos/wasmd/proto/cosmwasm/wasm/v1/* "$COSMOS_SDK_DIR/proto/cosmwasm/" || true

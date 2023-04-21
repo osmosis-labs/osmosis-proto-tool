2fd8d3f65067d3156482330cb5f6a00aabf93b5e
jonator <jonathanator0@gmail.com>
2023-04-21 09:23:11 -0500

    update gen coders script

--- 2-gen-coders.sh
+++ 2-gen-coders.sh
@@ -2,6 +2,14 @@
 set -o errexit -o nounset -o pipefail
 command -v shellcheck >/dev/null && shellcheck "$0"
 
+validate_proto_file() {
+  # File exists
+  if [ ! -f "$1" ]; then
+    echo "File not found: $1"
+    exit 1
+  fi
+}
+
 GENERATED_DIR="./tmp"
 ROOT_PROTO_DIR="./proto/cosmos/cosmos-sdk"
 COSMOS_PROTO_DIR="$ROOT_PROTO_DIR/proto/cosmos"
@@ -10,7 +18,34 @@ COSMWASM_PROTO_DIR="$ROOT_PROTO_DIR/proto/cosmwasm"
 TENDERMINT_PROTO_DIR="$ROOT_PROTO_DIR/third_party/proto/tendermint"
 OSMOSIS_PROTO_DIR="$ROOT_PROTO_DIR/proto/osmosis"
 
+PROTO_FILES=(
+  "$COSMOS_PROTO_DIR/base/v1beta1/coin.proto"
+  "$COSMOS_PROTO_DIR/bank/v1beta1/bank.proto"
+  "$OSMOSIS_PROTO_DIR/poolmanager/v1beta1/tx.proto"
+  "$OSMOSIS_PROTO_DIR/poolmanager/v1beta1/swap_route.proto"
+  "$OSMOSIS_PROTO_DIR/gamm/pool-models/balancer/balancerPool.proto"
+  "$OSMOSIS_PROTO_DIR/gamm/pool-models/balancer/tx/tx.proto"
+  "$OSMOSIS_PROTO_DIR/gamm/pool-models/stableswap/stableswap_pool.proto"
+  "$OSMOSIS_PROTO_DIR/gamm/pool-models/stableswap/tx.proto"
+  "$OSMOSIS_PROTO_DIR/concentrated-liquidity/pool-model/tx.proto"
+  "$OSMOSIS_PROTO_DIR/concentrated-liquidity/incentive_record.proto"
+  "$OSMOSIS_PROTO_DIR/concentrated-liquidity/pool.proto"
+  "$OSMOSIS_PROTO_DIR/concentrated-liquidity/position.proto"
+  "$OSMOSIS_PROTO_DIR/concentrated-liquidity/tickInfo.proto"
+  "$OSMOSIS_PROTO_DIR/concentrated-liquidity/tx.proto"
+  "$OSMOSIS_PROTO_DIR/superfluid/tx.proto"
+  "$OSMOSIS_PROTO_DIR/lockup/lock.proto"
+  "$OSMOSIS_PROTO_DIR/lockup/tx.proto"
+  "$OSMOSIS_PROTO_DIR/incentives/tx.proto"
+)
+
 mkdir -p "$GENERATED_DIR"
+
+# Validate each proto file
+for proto_file in "${PROTO_FILES[@]}"; do
+  validate_proto_file "$proto_file"
+done
+
 yarn pbjs \
   -t static-module \
   --es6 \
@@ -21,27 +56,4 @@ yarn pbjs \
   --no-verify \
   --no-delimited \
   --force-long \
-  "$COSMOS_PROTO_DIR/base/v1beta1/coin.proto" \
-  "$COSMOS_PROTO_DIR/base/query/v1beta1/pagination.proto" \
-  "$COSMOS_PROTO_DIR/bank/v1beta1/bank.proto" \
-  "$OSMOSIS_PROTO_DIR/poolmanager/v1beta1/tx.proto" \
-  "$OSMOSIS_PROTO_DIR/poolmanager/v1beta1/swap_route.proto" \
-  "$OSMOSIS_PROTO_DIR/gamm/pool-models/balancer/balancerPool.proto" \
-  "$OSMOSIS_PROTO_DIR/gamm/pool-models/balancer/tx/tx.proto" \
-  "$OSMOSIS_PROTO_DIR/gamm/pool-models/stableswap/stableswap_pool.proto" \
-  "$OSMOSIS_PROTO_DIR/gamm/pool-models/stableswap/tx.proto" \
-  "$OSMOSIS_PROTO_DIR/concentrated-liquidity/pool-model/query.proto" \
-  "$OSMOSIS_PROTO_DIR/concentrated-liquidity/pool-model/tx.proto" \
-  "$OSMOSIS_PROTO_DIR/concentrated-liquidity/incentive_record.proto" \
-  "$OSMOSIS_PROTO_DIR/concentrated-liquidity/pool.proto" \
-  "$OSMOSIS_PROTO_DIR/concentrated-liquidity/position.proto" \
-  "$OSMOSIS_PROTO_DIR/concentrated-liquidity/tickInfo.proto" \
-  "$OSMOSIS_PROTO_DIR/concentrated-liquidity/tx.proto" \
-  "$OSMOSIS_PROTO_DIR/superfluid/tx.proto" \
-  "$OSMOSIS_PROTO_DIR/lockup/lock.proto" \
-  "$OSMOSIS_PROTO_DIR/lockup/tx.proto" \
-  "$OSMOSIS_PROTO_DIR/incentives/tx.proto" \
-
-# Work around https://github.com/protobufjs/protobuf.js/issues/1477
-# shellcheck disable=SC2016
-sed -i "" -e 's/^const \$root =.*$/const \$root = {};/' "$GENERATED_DIR/codecimpl.js"
+  "${PROTO_FILES[@]}"
